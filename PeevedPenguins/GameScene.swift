30c04cc4ef5771144baa93b5c7bdf1aefef8c2ae
NormaTu545 <NormaTu545@gmail.com>
2016-06-28 14:57:44 -0700

    Penguins' trajectory fixed
    a force is applied to project the penguins like thrown balls.
    
    Penguin weightL 0.25
    Force had direction x200

--- PeevedPenguins/GameScene.swift
+++ PeevedPenguins/GameScene.swift
@@ -26,6 +26,13 @@ class GameScene: SKScene {
         
         /* Move penguin to the catapult bucket area */
         penguin.avatar.position = catapultArm.position + CGPoint(x: 32, y: 50)
+        
+        /* Impulse vector */
+        let launchDirection = CGVector(dx: 1, dy: 0)
+        let force = launchDirection * 200 //makes sure penguins have enough force to fly
+        
+        /* Apply impulse to penguin */
+        penguin.avatar.physicsBody?.applyImpulse(force)
     }
     
     override func update(currentTime: CFTimeInterval) {
